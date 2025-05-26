a5a705064335abcbfc9b33b5cb2f9eb8eb98174a
martinFL <martin0022f@gmail.com>
2025-05-26 19:59:55 -0300

    Creacion Clase Usuario y EstadoUsuario. Sin implementacion ni test

---
+++ src/tpFinal/EstadoUsuario.java
@@ -0,0 +1,7 @@
+package tpFinal;
+
+public abstract class EstadoUsuario {
+	public abstract void SubirMuestra(Muestra m, Usuario u);
+	public abstract void opinar(Muestra m, Vinchuca v, Usuario u);
+	
+}
--- src/tpFinal/Usuario.java
+++ src/tpFinal/Usuario.java
@@ -1,17 +1,35 @@
 package tpFinal;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+
 public abstract class Usuario {
 	private int id;
 	private boolean esExperto;
+	private EstadoUsuario estado;
+	HashMap<Muestra, LocalDate> envios;
+	HashMap<Muestra, LocalDate> opiniones;
 	
-	public abstract void SubirMuestra(Muestra m);
+	public void SubirMuestra(Muestra m) {
+		estado.SubirMuestra(m,this);
+	}
+	
 	public void opinar(Muestra m, Vinchuca v) {
-		m.agregarOpinion(v, this,m);
+		estado.opinar(m,v,this);
+	}
+	
+	public int cantidadDeOpiniones(){
+		return 0; //Implementar
+	};
+	
+	public int cantidadDeEnvios() {
+		return 0; //Implementar
 	}
 	
 	public boolean isEsExperto() {
 		return esExperto;
 	}
+	
 	public void setEsExperto(boolean esExperto) {
 		this.esExperto = esExperto;
 	}
@@ -19,7 +37,5 @@ public abstract class Usuario {
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+	
 }
