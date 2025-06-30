2ad3476a5bf16a0c59fc3c45a763cb24e5edf10c
martinFL <martin0022f@gmail.com>
2025-06-30 12:38:33 -0300

    add list opinionesExpertos

--- src/tpFinal/EstadoMuestraProcesoExperto.java
+++ src/tpFinal/EstadoMuestraProcesoExperto.java
@@ -1,9 +1,19 @@
 package tpFinal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EstadoMuestraProcesoExperto extends EstadoMuestra {
-	private List<Vinchuca> opinionesExpertos;
+	private List<Vinchuca> opinionesExpertos; 
+	//Para contabilizar las opiniones de expertos
+	
+	public EstadoMuestraProcesoExperto() {
+		this.opinionesExpertos = new ArrayList<>();
+	}
+	
+	private void addOpinion(Vinchuca v) {
+		this.opinionesExpertos.add(v);
+	}
 	
     @Override
     public boolean esVerificada() {
@@ -15,13 +25,15 @@ public class EstadoMuestraProcesoExperto extends EstadoMuestra {
 		// TODO Auto-generated method stub
 		if(u.isEsExperto() && m.fueVotado(v)) {
 			//m.historial.put(v, (m.obtenerVotosDe(v))+1);
-			m.setEstadoMuestra(new EstadoMuestraProcesoVerificado());
 			m.realizarOpinion(v);
+			addOpinion(v);
 			calcularResultado(m);
+			m.setEstadoMuestra(new EstadoMuestraProcesoVerificado());
 			//m.opinion = m.obtenerVinchucaConMasVotos();
 			//m.actualizarUltimaVotacion();
 		}else{
 			if(u.isEsExperto()) {
+				addOpinion(v);
 				m.realizarOpinion(v);
 				//m.historial.put(v, (m.obtenerVotosDe(v))+1);
 				//m.actualizarUltimaVotacion();
@@ -33,6 +45,6 @@ public class EstadoMuestraProcesoExperto extends EstadoMuestra {
 
 	@Override
 	public void calcularResultado(Muestra m) {
-	// Aca se debe calcular con la lista de opinionesExpertos y hacerle el setOpinion a esa muestra
+		// Aca se debe calcular con la lista de opinionesExpertos y hacerle el setOpinion a esa muestra
 	}
 }
